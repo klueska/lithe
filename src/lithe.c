623695283e2a4ee83c8a5ad1886502379c824e2a
Kevin Klues <klueska@cs.berkeley.edu>
2012-11-12 17:46:39 -0800

    Update to include newest parlib changes

--- src/lithe.c
+++ src/lithe.c
@@ -130,7 +130,7 @@ static int __attribute__((constructor)) lithe_lib_init()
 
   /* Once we have set things up for the main context, initialize the uthread
    * library with that main context */
-  assert(!uthread_lib_init((uthread_t*)context));
+  assert(!parlib_init((uthread_t*)context));
 
   /* Now that the library is initialized, a TLS should be set up for this
    * context, so set it's current_sched var */
