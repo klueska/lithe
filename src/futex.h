306671609e1d70e01fa13f9fa2072ab7a985a19b
Andrew Waterman <waterman@cs.berkeley.edu>
2014-08-06 14:24:46 -0700

    Make futex.h suitable for C++

--- src/futex.h
+++ src/futex.h
@@ -6,7 +6,15 @@
 #define FUTEX_WAIT 0
 #define FUTEX_WAKE 1
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int futex(int *uaddr, int op, int val, const struct timespec *timeout,
           int *uaddr2, int val3);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* _FUTEX_H */
